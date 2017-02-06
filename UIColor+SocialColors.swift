09ae9fceb259c24871035d385f55ad10747e8e5b
Szymon Maślanka <szymon.maslanka@intive.com>
2017-02-06 09:52:14 +0100

    added swift version

---
+++ UIColor+SocialColors.swift
@@ -0,0 +1,50 @@
+//
+//  UIColor+SocialColors.m
+//
+//  Created by nahive on 04/02/17.
+//  Copyright © 2017 nahive. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+	private static func rgb(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat) -> UIColor {
+		return UIColor(red: red, green: green, blue: blue, alpha: 1)
+	}
+
+	static let socialColorFacebook = rgb(59, 89, 153)
+	static let socialColorTwitter = rgb(85,172,238)
+	static let socialColorLinkedIn = rgb(0,119,181)
+	static let socialColorSkype = rgb(0,175,240)
+	static let socialColorDropbox = rgb(0,126,229)
+	static let socialColorWordpress = rgb(33,117,155)
+	static let socialColorVimeo = rgb(26,183,234)
+	static let socialColorSlideShare = rgb(0,119,181)
+	static let socialColorVK = rgb(76,117,163)
+	static let socialColorTumblr = rgb(52,70,93)
+	static let socialColorYahoo = rgb(65,0,147)
+	static let socialColorGooglePlus = rgb(221,75,57)
+	static let socialColorPinterest = rgb(189,8,28)
+	static let socialColorYoutube = rgb(205,32,31)
+	static let socialColorStumbleUpon = rgb(235,73,36)
+	static let socialColorReddit = rgb(255,87,0)
+	static let socialColorQuora = rgb(185,43,39)
+	static let socialColorYelp = rgb(175,6,6)
+	static let socialColorWeibo = rgb(223,32,41)
+	static let socialColorProductHunt = rgb(218,85,47)
+	static let socialColorHackerNews = rgb(255,102,0)
+	static let socialColorSoundcloud = rgb(255,51,0)
+	static let socialColorBlogger = rgb(245,125,0)
+	static let socialColorWhatsApp = rgb(37,211,102)
+	static let socialColorWeChat = rgb(9,184,62)
+	static let socialColorLine = rgb(0,195,0)
+	static let socialColorMedium = rgb(2,184,117)
+	static let socialColorVine = rgb(0,180,137)
+	static let socialColorSlack = rgb(58,175,133)
+	static let socialColorInstagram = rgb(228,64,95)
+	static let socialColorDribbble = rgb(234,76,137)
+	static let socialColorFlickr = rgb(255,0,132)
+	static let socialColorFourSquare = rgb(249,72,119)
+	static let socialColorBehance = rgb(19,20,24)
+	static let socialColorSnapChat = rgb(255,252,0);
+}
